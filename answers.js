8d1305ba416304430dd252f0d9562f9767f4e78c
MattyChance <chenmatty.wang@gmail.com>
2016-10-06 18:29:42 +0000

    added exercise 2 using filter method

--- answers.js
+++ answers.js
@@ -1,15 +1,23 @@
-//Exercise one: 
-function printPositives(arr) {
-    var newArr = [];
-    arr.forEach(function(num) {
-        // num > 0 ? console.log(num) : arr.splice(arr.indexOf(num), 1);
-        if (num > 0) {
-            newArr.push(num);
-        }
-    });
-    return newArr;
-
-}
+//Exercise 1: 
+// function printPositives(arr) {
+//     var newArr = [];
+//     arr.forEach(function(num) {
+//         // num > 0 ? newArr.push(num) : false;
+//         if (num > 0) {
+//             newArr.push(num);
+//         }
+//     });
+//     return newArr;
+// }
 // var myArr = [12, -3, -4, 18];
 // console.log(printPositives(myArr));
 
+//Exercise 2: do the same thing using filter
+function getPositives(arr) {
+    var newArr = arr.filter(function(num) {
+        return num > 0;
+    });
+    return newArr;
+}
+var myArr = [12, -3, -4, 18];
+console.log(getPositives(myArr));
