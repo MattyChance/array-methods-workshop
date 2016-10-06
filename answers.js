edb664131c44895e6fbfe6321fee41e5c2bdfeb1
MattyChance <chenmatty.wang@gmail.com>
2016-10-06 18:41:24 +0000

    added exercise 2B

--- answers.js
+++ answers.js
@@ -1,14 +1,14 @@
-//Exercise 1: 
-// function printPositives(arr) {
-//     var newArr = [];
-//     arr.forEach(function(num) {
-//         // num > 0 ? newArr.push(num) : false;
-//         if (num > 0) {
-//             newArr.push(num);
-//         }
-//     });
-//     return newArr;
-// }
+// Exercise 1: 
+function printPositives(arr) {
+    var newArr = [];
+    arr.forEach(function(num) {
+        // num > 0 ? newArr.push(num) : false;
+        if (num > 0) {
+            newArr.push(num);
+        }
+    });
+    return newArr;
+}
 // var myArr = [12, -3, -4, 18];
 // console.log(printPositives(myArr));
 
@@ -19,5 +19,15 @@ function getPositives(arr) {
     });
     return newArr;
 }
-var myArr = [12, -3, -4, 18];
-console.log(getPositives(myArr));
+// var myArr = [12, -3, -4, 18];
+// console.log(getPositives(myArr));
+
+//Exercise 2B: Filter array, print numbers from the filtered array
+var myArr = [23, 5, -3, -8, 0, 1];
+
+function keepPositive (arr) {
+    var filteredArr = arr.filter(function(num) {return num > 0});
+    return filteredArr.forEach(function(num) {console.log(num)});
+}
+var output = keepPositive(myArr);
+console.log(output);
